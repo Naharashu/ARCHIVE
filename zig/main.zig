1ca86651a408d1860d6b6545736247f5ed16e49b
Naharashu <130159705+Naharashu@users.noreply.github.com>
2025-07-07 22:33:42 +0300

    Add files via upload

--- zig/main.zig
+++ zig/main.zig
@@ -1,5 +1,20 @@
-const utils = @import("test.zig");
+const std = @import("std");
+const builtin = @import("builtin");
+
+pub fn ssr(code: i128) !i128 {
+    const x: i128 = code + 1;
+    const y: i128 = std.time.nanoTimestamp();
+    const z = code ^ (101302032103 + y) << 3;
+    var res: i128 = (x ^ (code + 23)) ^ (y ^ z) - 10120 ^ 132;
+    res = (res - 2833284723) ^ 138;
+    if (res == 0) {
+        std.debug.print("Error in ssr\n", .{});
+        return -1;
+    }
+    return res ^ y;
+}
 
 pub fn main() !void {
-    utils.println("Hello from main using utils", .{});
+    const x = ssr(1);
+    std.debug.print("{any}\n", .{x});
 }
